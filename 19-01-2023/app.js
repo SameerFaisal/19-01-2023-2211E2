9d9ef6d742faadb785eb7e1eec6f233614f0a4b1
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-19 17:04:57 +0500

    19-01-2023

---
+++ 19-01-2023/app.js
@@ -0,0 +1,124 @@
+// function minus(){
+//    let num1=2
+//     var num2=1
+//     var result=num1-num2
+//     alert(result)
+// }
+
+
+
+// function display(){
+//     let stdName="John"
+// }
+// alert(stdName)
+
+
+// function add(){
+//     let num1=+prompt("enter a number")
+//     var num2=+prompt("enter a number")
+//     alert(num1+num2)
+// }
+// function subt(){
+//     let num1=+prompt("enter a number")
+//     var num2=+prompt("enter a number")
+//     alert(num1-num2)
+// }
+// function mul(){
+//     let num1=+prompt("enter a number")
+//     var num2=+prompt("enter a number")
+//     alert(num1*num2)
+// }
+// function div(){
+//     let num1=+prompt("enter a number")
+//     var num2=+prompt("enter a number")
+//     alert(num1/num2)
+// }
+
+
+// let choice=+prompt("Enter 1 to add, 2 to subtract, 3 to multiply and 4 to divide")
+// switch(choice){
+//     case 1:
+//         add()
+//         break
+//     case 2:
+//         subt()
+//         break
+//     case 3:
+//         mul()
+//         break
+//     case 4:
+//         div()
+//         break
+//     default:
+//         alert("Invalid Input")
+//         break
+// }
+// let num1=+prompt("enter a number")
+// var num2=+prompt("enter a number")
+
+// function add(n1,n2){   //Parameters
+   
+//     alert(parseFloat(n1)+parseFloat(n2))
+// }
+// function subt(n1,n2){
+   
+//     alert(n1-n2)
+// }
+// function mul(){
+   
+//     alert(num1*num2)
+// }
+// function div(){
+   
+//     alert(num1/num2)
+// }
+
+
+// let choice=+prompt("Enter 1 to add, 2 to subtract, 3 to multiply and 4 to divide")
+// switch(choice){
+//     case 1:
+//         add(num1,num2)  //Arguments
+//         break
+//     case 2:
+//         subt()
+//         break
+//     case 3:
+//         mul()
+//         break
+//     case 4:
+//         div()
+//         break
+//     default:
+//         alert("Invalid Input")
+//         break
+// }
+
+// var phones=["OPPO","Samsung","Vivo","Apple"]
+
+// document.write("<select>")
+// for(let i=0;i<phones.length;i++){
+//     document.write("<option>"+phones[i]+"</option>")
+// }
+// document.write("</select>")
+
+// document.write("<ol>")
+// for(let i in phones){
+//     document.write("<li>"+phones[i]+"</li>")
+// }
+// document.write("</ol>")
+
+
+// let fname=prompt("Enter First Name")
+// let lname=prompt("Enter Last Name")
+
+
+// function display(fn,ln){
+// let result=fn+ln
+// return result
+// }
+
+// let result=display(fname,lname)
+// let result1=display(lname,fname)
+// alert(result)
+// alert(result1)
+
